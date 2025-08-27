be1adb166fe027062064778ce0aa207f2554be41
unknown <marcosrrodriguescastro@gmail.com>
2025-08-27 20:14:19 -0300

    rretgwa

---
+++ src/main/java/com/projetofesta/festudo/FestudoApplication.java
@@ -0,0 +1,13 @@
+package com.projetofesta.festudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestudoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FestudoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetofesta/festudo/FestudoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetofesta.festudo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestudoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
